f58aa1bfc5378c92d3d6404ee17a8303cd322e02
Richard Hull <rm_hull@yahoo.co.uk>
2015-02-16 00:04:43 +0000

    Setup changes after rename

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     keywords = "raspberry pi rpi led max7219 matrix seven segment",
     url = "https://github.com/rm-hull/max7219",
     packages=['max7219'],
-    ext_modules=[Extension('max7219.spi',['src/spi.c'])]
+    ext_modules=[Extension('max7219.spi',['max7219/spi.c'])]
 )
