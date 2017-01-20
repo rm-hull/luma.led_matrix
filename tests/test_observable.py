d37265852b24c4ddbe3e3a9fec5e184750bf5d22
Richard Hull <rm_hull@yahoo.co.uk>
2017-01-20 01:52:25 +0000

    Additional tests

---
+++ tests/test_observable.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014-17 Richard Hull and contributors
+# See LICENSE.rst for details.
+
+
+from luma.led_matrix.device import observable
+
+
+class test_bell(object):
+
+    called = 0
+
+    def ding(self, *args):
+        self.called += 1
+
+
+def test_length():
+    bell = test_bell()
+    buf = observable(bytearray("hello", "utf-8"), bell.ding)
+    assert len(buf) == 5
+    assert bell.called == 1
+
+
+def test_iteration():
+    bell = test_bell()
+    buf = observable(bytearray("hello", "utf-8"), bell.ding)
+    assert list(iter(buf)) == [ord(ch) for ch in "hello"]
+    assert bell.called == 1
+
+
+def test_getattribute():
+    bell = test_bell()
+    buf = observable(bytearray("hello", "utf-8"), bell.ding)
+    assert list(iter(buf.decode("utf-8"))) == list("hello")
+    assert bell.called == 1
+
+
+def test_getitem():
+    bell = test_bell()
+    buf = observable(bytearray("hello", "utf-8"), bell.ding)
+    assert buf[2] == ord("l")
+    assert bell.called == 1
+
+
+def test_setitem():
+    bell = test_bell()
+    buf = observable(bytearray("hello", "utf-8"), bell.ding)
+    buf[0] = "y"
+    assert str(buf) == "yello"
+    assert bell.called == 2
+
+
+def test_delitem():
+    bell = test_bell()
+    buf = observable(bytearray("hello", "utf-8"), bell.ding)
+    del buf[4]
+    assert str(buf) == "hell"
+    assert bell.called == 2
+
+
+def test_getslice():
+    bell = test_bell()
+    buf = observable(bytearray("hello", "utf-8"), bell.ding)
+    assert buf[2:4] == bytearray("ll", "utf-8")
+    assert bell.called == 1
+
+
+def test_str():
+    bell = test_bell()
+    buf = observable(bytearray("hello", "utf-8"), bell.ding)
+    assert str(buf) == "hello"
+    assert bell.called == 1
+
+
+def test_repr():
+    bell = test_bell()
+    buf = observable(bytearray("hello", "utf-8"), bell.ding)
+    assert repr(buf) == "bytearray(b'hello')"
+    assert bell.called == 1
---
+++ tests/test_sevensegment.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2014-17 Richard Hull and contributors
+# See LICENSE.rst for details.
+
+import pytest
+import hashlib
+import os.path
+from tempfile import NamedTemporaryFile
+
+from PIL import Image
+from luma.led_matrix.device import sevensegment
+from luma.core.emulator import dummy, capture
+
+
+def md5(fname):
+    with open(fname, 'rb') as fp:
+        return hashlib.md5(fp.read()).hexdigest()
+
+
+def test_init():
+    device = dummy(width=24, height=8, mode="1", transform="none")
+    sevensegment(device)
+    assert device.image == Image.new("1", (24, 8))
+
+
+def test_overflow():
+
+    device = dummy(width=24, height=8, mode="1", transform="none")
+    seg = sevensegment(device)
+    with pytest.raises(OverflowError) as ex:
+        seg.text = "This is too big to fit in 3x8 seven-segment displays"
+    assert str(ex.value) == "Device's capabilities insufficent for value 'This is too big to fit in 3x8 seven-segment displays'"
+
+
+def test_setter_getter():
+    reference = os.path.abspath(os.path.join(
+        os.path.dirname(__file__),
+        'reference',
+        'golden_ratio.png'))
+
+    fname = NamedTemporaryFile(suffix=".png").name
+    print(fname)
+    device = capture(file_template=fname, width=24, height=8, mode="1", transform="none")
+    seg = sevensegment(device)
+    seg.text = "1.61803398875"
+    assert seg.text.decode("utf-8") == "1.61803398875"
+    assert md5(reference) == md5(fname)
