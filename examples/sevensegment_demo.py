e3cba0cbc60f1017a17e2289a6a053a6bb231bc9
Richard Hull <rm_hull@yahoo.co.uk>
2017-01-21 23:59:09 +0000

    Refresh 7-segment demo - fixes #54

--- examples/sevensegment_demo.py
+++ examples/sevensegment_demo.py
@@ -1,124 +1,116 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 Richard Hull and contributors
+# See LICENSE.rst for details.
 
 """
 Example for seven segment displays.
 """
 
 import time
-import random
 from datetime import datetime
 
-import max7219.led as led
+from luma.led_matrix.device import max7219, sevensegment
+from luma.core.serial import spi
+from luma.core.virtual import viewport
 
 
-def date(device, deviceId):
+def date(seg):
     """
     Display current date on device.
     """
     now = datetime.now()
-    day = now.day
-    month = now.month
-    year = now.year - 2000
-
-    # Set day
-    device.letter(deviceId, 8, int(day / 10))     # Tens
-    device.letter(deviceId, 7, day % 10)          # Ones
-    device.letter(deviceId, 6, '-')               # dash
-    # Set month
-    device.letter(deviceId, 5, int(month / 10))     # Tens
-    device.letter(deviceId, 4, month % 10)     # Ones
-    device.letter(deviceId, 3, '-')               # dash
-    # Set year
-    device.letter(deviceId, 2, int(year / 10))     # Tens
-    device.letter(deviceId, 1, year % 10)     # Ones
+    seg.text = now.strftime("%y-%m-%d")
 
 
-def clock(device, deviceId, seconds):
+def clock(seg, seconds):
     """
     Display current time on device.
     """
-    for _ in range(seconds):
+    interval = 0.5
+    for i in range(int(seconds / interval)):
         now = datetime.now()
-        hour = now.hour
-        minute = now.minute
-        second = now.second
-        dot = second % 2 == 0                # calculate blinking dot
-        # Set hours
-        device.letter(deviceId, 4, int(hour / 10))     # Tens
-        device.letter(deviceId, 3, hour % 10, dot)     # Ones
-        # Set minutes
-        device.letter(deviceId, 2, int(minute / 10))   # Tens
-        device.letter(deviceId, 1, minute % 10)        # Ones
-        time.sleep(1)
+        seg.text = now.strftime("%H-%M-%S")
+
+        # calculate blinking dot
+        if i % 2 == 0:
+            seg.text = now.strftime("%H-%M-%S")
+        else:
+            seg.text = now.strftime("%H %M %S")
+
+        time.sleep(interval)
+
+
+def show_message_vp(device, msg, delay=0.1):
+    # Implemented with virtual viewport
+    width = device.width
+    padding = " " * width
+    msg = padding + msg + padding
+    n = len(msg)
+
+    virtual = viewport(device, width=n, height=8)
+    sevensegment(virtual).text = msg
+    for i in reversed(list(range(n - width))):
+        virtual.set_position((i, 0))
+        time.sleep(delay)
+
+
+def show_message_alt(seg, msg, delay=0.1):
+    # Does same as above but does string slicing itself
+    width = seg.device.width
+    padding = " " * width
+    msg = padding + msg + padding
+
+    for i in range(len(msg)):
+        seg.text = msg[i:i + width]
+        time.sleep(delay)
 
 
 def main():
     # create seven segment device
-    device = led.sevensegment(cascaded=1)
+    serial = spi()
+    device = max7219(serial, cascaded=1)
+    seg = sevensegment(device)
 
     print('Simple text...')
     for _ in range(8):
-        device.write_text(0, "HELLO")
+        seg.text = "HELLO"
         time.sleep(0.6)
-        device.write_text(0, " GOODBYE")
+        seg.text = " GOODBYE"
+        time.sleep(0.6)
+
+    # Digit slicing
+    print("Digit slicing")
+    seg.text = "_" * seg.device.width
+    time.sleep(1.0)
+
+    for i, ch in enumerate([9,8,7,6,5,4,3,2]):
+        seg.text[i] = str(ch)
+        time.sleep(0.6)
+
+    for i in range(len(seg.text)):
+        del seg.text[0]
         time.sleep(0.6)
 
     # Scrolling Alphabet Text
     print('Scrolling alphabet text...')
-    device.show_message("HELLO EVERYONE!")
-    device.show_message("0123456789 abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    show_message_vp(device, "HELLO EVERYONE!")
+    show_message_vp(device, "PI is 3.14159 ... ")
+    show_message_vp(device, "IP is 127.0.0.1 ... ")
+    show_message_alt(seg, "0123456789 abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
     # Digit futzing
-    date(device, 0)
+    date(seg)
     time.sleep(5)
-    clock(device, 0, seconds=10)
+    clock(seg, seconds=10)
 
     # Brightness
     print('Brightness...')
     for x in range(5):
         for intensity in range(16):
-            device.brightness(intensity)
+            seg.device.contrast(intensity * 16)
             time.sleep(0.1)
-    device.brightness(7)
-
-    # Scrolling
-    print('Scrolling...')
-    for x in range(2):
-        for _ in range(8):
-            device.scroll_right()
-            time.sleep(0.1)
-        time.sleep(1)
-    device.clear()
-
-    # Negative numbers
-    print('Negative numbers...')
-    for x in range(-30, 128):
-        device.write_number(deviceId=0, value=x)
-        time.sleep(0.05)
-
-    # Hex numbers
-    print('Hex numbers...')
-    for x in range(0xfa909, 0xfab2a):
-        device.write_number(deviceId=0, value=x, base=16, leftJustify=True)
-        time.sleep(0.025)
-
-    print('Clear device...')
-    device.clear()
-    time.sleep(1)
-
-    print('Random numbers...')
-    a = random.randint(-999, 999)
-    # b = random.randint(-3223, 999)
-
-    device.brightness(3)
-    for x in range(500):
-        a += random.random() * 10
-        # b -= 1
-        # c = a + b / (random.random() * 43)
-        device.write_number(deviceId=0, value=a, decimalPlaces=1)
-        # device.write_number(deviceId=1, value=b, zeroPad=True)
-        # device.write_number(deviceId=2, value=c, decimalPlaces=2)
-        time.sleep(0.5)
+    device.contrast(0x7F)
 
 
 if __name__ == '__main__':
