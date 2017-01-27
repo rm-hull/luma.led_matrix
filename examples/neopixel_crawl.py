80fbee0603395485ec1345447326410e19425db8
Richard Hull <rm_hull@yahoo.co.uk>
2017-01-27 22:58:20 +0000

    Add pixel crawler example

---
+++ examples/neopixel_crawl.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 Richard Hull and contributors
+# See LICENSE.rst for details.
+
+import time
+
+from luma.led_matrix.device import neopixel
+from luma.core.render import canvas
+
+device = neopixel(cascaded=32)
+
+for i in range(device.cascaded):
+    with canvas(device) as draw:
+        draw.point((i, 0), fill="green")
+    time.sleep(0.5)
