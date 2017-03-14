3f43684681d12ae87eddc882c0112354579637c8
Thijs Triemstra <info@collab.nl>
2017-03-14 01:23:52 +0100

    add deprecation warning for legacy module

--- luma/led_matrix/legacy.py
+++ luma/led_matrix/legacy.py
@@ -2,15 +2,17 @@
 # Copyright (c) 2017 Richard Hull and contributors
 # See LICENSE.rst for details.
 
-# FIXME: To be removed when 0.6.0 released
-
+from luma.core.util import deprecation
 from luma.core.legacy import text, textsize, show_message
-from luma.core.legacy.font import proportional, CP437_FONT, SINCLAIR_FONT, \
-    LCD_FONT, UKR_FONT, TINY_FONT, DEFAULT_FONT
+from luma.core.legacy.font import (proportional, CP437_FONT, SINCLAIR_FONT,
+    LCD_FONT, UKR_FONT, TINY_FONT, DEFAULT_FONT)
 
-print("\nWARNING! 'luma.led_matrix.legacy' is now deprecated and will be removed in\n"
-      "         the next major release. Please use the equivalent functionality from\n"
-      "         'luma.core.legacy' and 'luma.core.legacy.font'.\n")
+
+# trigger DeprecationWarning
+deprecation_msg = ("WARNING! 'luma.led_matrix.legacy' is now deprecated and will be removed in "
+      "v1.0.0. Please use the equivalent functionality from "
+      "'luma.core.legacy' and 'luma.core.legacy.font'.")
+deprecation(deprecation_msg)
 
 text
 textsize
