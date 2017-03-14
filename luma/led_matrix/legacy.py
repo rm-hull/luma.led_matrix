e813ed47f3a18a2b00dd1dc1336a2ee93e5e749e
Richard Hull <rm_hull@yahoo.co.uk>
2017-03-14 00:59:17 +0000

    Remove refs & add noqa pragma
    Should improve code coverage

--- luma/led_matrix/legacy.py
+++ luma/led_matrix/legacy.py
@@ -3,9 +3,8 @@
 # See LICENSE.rst for details.
 
 from luma.core.util import deprecation
-from luma.core.legacy import text, textsize, show_message
-from luma.core.legacy.font import (proportional, CP437_FONT, SINCLAIR_FONT,
-    LCD_FONT, UKR_FONT, TINY_FONT, DEFAULT_FONT)
+from luma.core.legacy import text, textsize, show_message  # noqa: F401
+from luma.core.legacy.font import proportional, CP437_FONT, SINCLAIR_FONT, LCD_FONT, UKR_FONT, TINY_FONT, DEFAULT_FONT  # noqa: F401
 
 
 # trigger DeprecationWarning
@@ -14,13 +13,4 @@ deprecation_msg = ("WARNING! 'luma.led_matrix.legacy' is now deprecated and will
       "'luma.core.legacy' and 'luma.core.legacy.font'.")
 deprecation(deprecation_msg)
 
-text
-textsize
-show_message
-proportional
-CP437_FONT
-SINCLAIR_FONT
-LCD_FONT
-UKR_FONT
-TINY_FONT
-DEFAULT_FONT
+pass
