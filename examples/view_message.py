27ca127d50f61b6206bf0289710a11b0550b5d5c
Thomas Wenzlaff <karotz@wenzlaff.de>
2020-07-04 11:05:28 +0200

    imported but unused

--- examples/view_message.py
+++ examples/view_message.py
@@ -9,10 +9,8 @@ import argparse
 
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
-from luma.core.render import canvas
-from luma.core.virtual import viewport
-from luma.core.legacy import text, show_message
-from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+from luma.core.legacy import show_message
+from luma.core.legacy.font import proportional, CP437_FONT
 
 
 def output(n, block_orientation, rotate, inreverse, text):
