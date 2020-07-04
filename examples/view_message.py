107d032cb9b121143c82be3818984bda60b355aa
Thomas Wenzlaff <karotz@wenzlaff.de>
2020-07-04 10:27:06 +0200

    Add import

--- examples/view_message.py
+++ examples/view_message.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 # Commandline Wrapper
 # Thomas Wenzlaff
- 
+# See LICENSE.rst for details.
+
+import re 
 import time
 import argparse
 
@@ -14,7 +16,8 @@ from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
 
-def output(n, block_orientation, rotate, inreverse, text): 
+def output(n, block_orientation, rotate, inreverse, text):
+    # create matrix device 
     serial = spi(port=0, device=0, gpio=noop())
     device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation,
                      rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
