7acec033bb08c3c4f144e753c5cd986da98f349b
Thomas Wenzlaff <karotz@wenzlaff.de>
2020-07-04 08:02:48 +0200

    New commandline wrapper

---
+++ examples/view_message.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# Commandline Wrapper
+# Thomas Wenzlaff
+ 
+import re
+import time
+import argparse
+
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+
+def output(n, block_orientation, rotate, inreverse, text): 
+
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial, cascaded=n or 1, block_orientation=block_orientation,
+                     rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
+    print(text)
+
+    show_message(device, text, fill="white", font=proportional(CP437_FONT), scroll_delay=0.05)
+    time.sleep(1)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='view_massage arguments',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--cascaded', '-n', type=int, default=1, help='Number of cascaded MAX7219 LED matrices')
+    parser.add_argument('--block-orientation', type=int, default=0, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
+    parser.add_argument('--rotate', type=int, default=0, choices=[0, 1, 2, 3], help='Rotate display 0=0°, 1=90°, 2=180°, 3=270°')
+    parser.add_argument('--reverse-order', type=bool, default=False, help='Set to true if blocks are in reverse order')
+    parser.add_argument('--text', '-t', default='>>> No text set', help='Set text message') 
+    args = parser.parse_args()
+
+    try:
+        output(args.cascaded, args.block_orientation, args.rotate, args.reverse_order, args.text)
+    except KeyboardInterrupt:
+        pass
+
